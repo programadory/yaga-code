98a396928528f6aaadda6bd6172dba1c1c3c78ec
programadory_android <programadory@gmail.com>
2023-12-16 03:37:19 -0300

    finished

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-#yaga-code-git
+# yaga-code-git
+
+# Optimized code, using better function to saver space and memory.
+
+# Implemment pointers in the future, cast with enum type. For now is finished.
--- yaga.cpp
+++ yaga.cpp
@@ -4,9 +4,13 @@
 
 using namespace std;
 
+const int BUFFER_ON=0, BUFFER_OFF=1;
+
+string strcatch_global="";
+
 bool tag_cmp(const char* cmd, const char* cmd2, const char* cmd_cmp, const char* cmd_cmp2);
 
-void yaga_main(bool TAG__COMMAND, string COMMAND, string ARG_COMPLETE, string ARG_X[], char* ARG_V[], int ARG_C, string OUTPUT_TXT);
+void yaga_main(bool TAG__COMMAND, string COMMAND, string ARG_COMPLETE, string ARG_X[], char* ARG_V[], int ARG_C, int enter_times, string OUTPUT_TXT);
 
 
 int main(int argc, char* argv[]){
@@ -18,41 +22,15 @@ int main(int argc, char* argv[]){
   if(!strcmp(argv[1],"-v")){
     system("g++ -v");
   }
- 
-  if((!strcmp(argv[1],"github")) && (!strcmp(argv[2], "-n")))
-  {
- 
-    cout << "Creating new local project and uploading to github\n";
-    
-    //Making dir project 
-    
-    arg_cmpl = "-code/";
-    argx[0] = "mkdir "; 
-    argx[1] = argv[3]+arg_cmpl;
-    argx[2] = argx[0]+argx[1];
-    
-    //Copying files to dir's project
-    
-    argx[3] = "cp --remove-destination ";
-    argx[4] = argv[3];
-    argx[5] = "* ";
-    argx[6] = argx[1];
-    argx[7] = argx[3]+argx[4]+argx[5]+argx[6];
-    
-    //Sending command's stream to system function
-    
-    cmd_stream = argx[2].c_str();
-    system(cmd_stream);
-    cmd_stream = argx[7].c_str();
-    system(cmd_stream);
-    
-    cout << "Project " << argx[1] << "has been created!\n";
-  }
   
-  yaga_main(tag_cmp(argv[1],argv[2],"-r",argv[2]), "rm -rf ", "-code/",argx, argv, argc, "Project has been removed!");
+  yaga_main(tag_cmp(argv[1],argv[2],"github","-n"), "mkdir ", "-code/", argx, argv, argc, BUFFER_ON, "Project has been created! - fist step!");
+  
+  yaga_main(tag_cmp(argv[1],argv[2],"github","-n"), "mv -v", "* ", argx, argv, argc, BUFFER_OFF, "");
+ 
+  yaga_main(tag_cmp(argv[1],argv[2],"-r",argv[2]), "rm -rf ", "-code/", argx, argv, argc, BUFFER_OFF,  "Project has been removed!");
  
   yaga_main(tag_cmp(argv[1],argv[2],"-o",argv[2]), "g++ -o ",
-  " *.cpp", argx, argv, argc, "Project has builded ok..");
+  " *.cpp", argx, argv, argc, BUFFER_OFF, "Project has builded ok..");
   
   
   return 0;
@@ -69,12 +47,12 @@ bool tag_cmp(const char* cmd, const char* cmd2, const char* cmd_cmp, const char*
 
 }
 
-void yaga_main(bool TAG__COMMAND, string COMMAND, string ARG_COMPLETE, string ARG_X[], char* ARG_V[], int ARG_C, string OUTPUT_TXT){
+void yaga_main(bool TAG__COMMAND, string COMMAND, string ARG_COMPLETE, string ARG_X[], char* ARG_V[], int ARG_C, int enter_times, string OUTPUT_TXT){
   
   const char* cmd_stream;
   
   if(TAG__COMMAND && ARG_C>1){
-    
+   
     ARG_X[0]=COMMAND;
     
     switch(ARG_C){
@@ -85,10 +63,14 @@ void yaga_main(bool TAG__COMMAND, string COMMAND, string ARG_COMPLETE, string AR
             ARG_X[1]=ARG_V[3]+ARG_COMPLETE;
             break;
     }
-   
-    ARG_X[2]=ARG_X[0]+ARG_X[1];
+    
+    ARG_X[2]=ARG_X[0]+ARG_X[1]+strcatch_global;
     cmd_stream = ARG_X[2].c_str();
     system(cmd_stream);
+    
+    if(enter_times==0){
+      strcatch_global = ARG_X[1];
+    }
     
     cout << OUTPUT_TXT << "\n";
   }
